d11fe9ee8144fbff4bc68e0e6819913fb91575f8
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-29 22:21:37 +0800

    Initial Commit

---
+++ Shared/MenuView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Liu Ziyi on 29/5/23.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView()
+    }
+}
---
+++ Shared/__swift_menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  __swift_menuApp.swift
+//  Shared
+//
+//  Created by Liu Ziyi on 29/5/23.
+//
+
+import SwiftUI
+
+@main
+struct __swift_menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MenuView()
+        }
+    }
+}
