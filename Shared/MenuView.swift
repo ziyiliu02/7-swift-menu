dffd901fecd49c9ec1ca26e7ae3fe7cb90bd00a9
Liu Ziyi <ziyi.liu02@gmail.com>
2023-05-31 23:20:06 +0800

    - Create an instance of DataService in MenuView and call it to get the menu data and display it when the list appears.

--- Shared/MenuView.swift
+++ Shared/MenuView.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct MenuView: View {
     
-    var menuItems:[MenuItem] = [MenuItem]()
+    @State var menuItems:[MenuItem] = [MenuItem]()
+    var dataService = DataService()
     
     var body: some View {
         
@@ -37,6 +38,10 @@ struct MenuView: View {
             
         }
         .listStyle(.plain)
+        .onAppear{
+            // Call for the data
+            menuItems = dataService.getData()
+        }
         
     }
 }
